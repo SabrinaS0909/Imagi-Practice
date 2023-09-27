566fec43eaac14b45462d51749d8df34c7cd7bee
Sabrina Spruck <113223660+SabrinaS0909@users.noreply.github.com>
2023-09-27 18:49:16 -0400

    very basic practice

---
+++ hello.py
@@ -0,0 +1,9 @@
+"""File is for practice purposes only, and will execute various simple lines of Python code.
+Created September 27, 2023. Version 1. Author: Sabrina Spruck"""
+
+# below is practice: printing in the console with Python
+print("Hello World!")
+print("Love you Python")
+print("Sabrina")
+print("Python")
+print("Java")
